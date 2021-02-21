4c2272abd25875b3da3c273f6429cb243af59362
Motiullah Sajit <sajitmotiullah@gamil.com>
2021-02-21 22:19:13 +0600

    update something

--- app.js
+++ app.js
@@ -6,18 +6,15 @@ const displayCountries = countries => {
     const countriesDiv = document.getElementById('countries');
     countries.forEach(country => {
         const countryDiv = document.createElement('div');
-        countryDiv.className = 'country';
+        countryDiv.className = 'col-lg-3 col-md-6 col-sm-12';
         const countryInfo = `
-        <div class="col">
-                    <div class="card">
+                    <div class="card h-100" onclick="displayCountryDetail('${country.name}')">
                     <img src="${country.flag}" alt="${country.name}">
                     <div class="card-body">
                         <h5 class="card-title">${country.name}</h5>
                         <p>${country.capital}</p>
-                        <button onclick="displayCountryDetail('${country.name}')" class="btn btn-outline-success">Details</button>
                     </div>
                     </div>
-            </div>
         `;
         countryDiv.innerHTML = countryInfo;
         countriesDiv.appendChild(countryDiv);
@@ -28,25 +25,8 @@ const displayCountryDetail = (name) => {
     const url = `https://restcountries.eu/rest/v2/name/${name}`;
     fetch(url)
         .then(res => res.json())
-        .then(data => renderCountryInfo(data[0]));
+        .then(data => detailsPopup(data[0]));
 }
-
-const renderCountryInfo = country => {
-    const countriesDiv = document.getElementById('countries');
-    countriesDiv.style.display = 'none';
-    const countryDiv = document.getElementById('country-detail');
-    countryDiv.innerHTML = `
-    <div class="card">
-    <img src="${country.flag}" alt="${country.name}">
-        <div class="card-body">
-        <h5 class="card-title">${country.name}</h5>
-        <p>Population: ${country.population}</p>
-        <p>Area: ${country.area}</p>
-        </div>
-    </div> `
-
-}
-
 
 function showBySearch() {
     let searchInput = document.getElementById('search-input');
@@ -57,13 +37,33 @@ function showBySearch() {
             countriesDiv.style.display = 'none';
             const countryDiv = document.getElementById('country-detail');
             countryDiv.innerHTML = `
-    <div class="card">
-    <img src="${data[0].flag}" alt="${data[0].name}">
-        <div class="card-body">
-        <h5 class="card-title">${data[0].name}</h5>
-        <p>Population: ${data[0].population}</p>
-        <p>Area: ${data[0].area}</p>
-        </div>
-    </div> `
+            <div class="card">
+            <img src="${data[0].flag}" alt="${data[0].name}">
+                <div class="card-body">
+                <h5 class="card-title">${data[0].name}</h5>
+                <p>Population: ${data[0].population}</p>
+                <p>Area: ${data[0].area}</p>
+                </div>
+            </div> `
         })
 };
+
+//details popUp
+
+function detailsPopup(country) {
+    document.getElementById("popupId").classList.toggle("active");
+
+    const insidePopuUp = document.getElementById('popUpContent');
+
+    insidePopuUp.innerHTML = `
+            <button class="close-btn" onclick="detailsPopup()"><i class="far fa-times-circle"></i></button>
+            <div class="card details-card">
+            <img class="img-fluid" src="${country.flag}" alt="${country.name}">
+                <div class="card-body">
+                <h5 class="card-title">${country.name}</h5>
+                <p>Population: ${country.population}</p>
+                <p>Area: ${country.area}</p>
+                </div>
+            </div> `
+}
+
